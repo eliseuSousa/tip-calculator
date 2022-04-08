eb732753e9c335d6867b25d7051fbee0a9026257
eliseuSousa <eliseusousa759@gmail.com>
2022-04-08 19:34:29 -0300

    Add focus ao porcentagem-custom

--- main.js
+++ main.js
@@ -1,17 +1,39 @@
+let porcentagens = document.querySelector(".porcentagens");
 let buttons = document.querySelectorAll(".porcentagem > button");
 
-buttons.forEach(function (key) {
-  key.addEventListener("click", function () {
-    removeStyles();
-    this.setAttribute("class", "botao focus");
-  });
+porcentagens.addEventListener("click", function(event) {
+  if (event.target == document.querySelector(".porcentagem-custom")) {
+    removeFocusButton();
+    addFocusInput();
+  } else {
+    removeFocusInput();
+    addFocusButton();
+  }
 });
 
-function removeStyles() {
+function addFocusInput() {
+  let input = document.querySelector(".porcentagem > input");
+  input.setAttribute("class", "campo-input porcentagem-custom focus");
+}
+
+function removeFocusInput() {
+  let input = document.querySelector(".porcentagem > input");
+  input.setAttribute("class", "campo-input porcentagem-custom");
+}
+
+function addFocusButton() {
+  buttons.forEach(function (key) {
+    key.addEventListener("click", function () {
+      removeFocusButton();
+      this.setAttribute("class", "botao focus");
+    });
+  });  
+}
+
+function removeFocusButton() {
   let button;
   for (let i = 0; i < buttons.length; i++) {
     button = document.querySelectorAll(".porcentagem > button")[i];
-    button.removeAttribute("focus");
     button.setAttribute("class", "botao padrao");
   }
 }
